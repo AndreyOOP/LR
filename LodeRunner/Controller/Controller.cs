e5c0c11770ef63bd43feb62a31619cc4a7dc57d8
AndreyOOP <andrew_yudaichev@i.ua>
2018-05-04 17:20:06 +0300

    Add sample how to test controller

--- LodeRunner/Controller/Controller.cs
+++ LodeRunner/Controller/Controller.cs
@@ -1,13 +1,30 @@
 ﻿namespace LodeRunner.Control
 {
     using LodeRunner.Model;
+    using LodeRunner.Model.SingleComponents;
 
     public class Controller
     {
         public Model Model { get; set; }
+        public View View { get; set; }
 
         public Controller()
         {
+        }
+
+        public char GetKeyInput()
+        {
+            return 'a';
+        }
+
+        public void OnKeyInput()
+        {
+            if(GetKeyInput() == 'a')
+            {
+                var player = Model.Get<Player>(ComponentType.Player);
+
+                player.X += 5;
+            }
         }
     }
 }
---
+++ LodeRunnerTests/Controller/ControllerTests.cs
@@ -0,0 +1,34 @@
+﻿namespace LodeRunner.Control.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using LodeRunner.Model;
+    using LodeRunner.Model.SingleComponents;
+
+    [TestClass()]
+    public class ControllerTests
+    {
+        Player player = new Player() { X = 10, Y = 0};
+        Model model = new Model();
+        Controller controller = new Controller();
+        View view = new View();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            model.Add(ComponentType.Player, player);
+
+            controller.Model = model;
+            controller.View = view;
+        }
+
+        [TestMethod()]
+        public void OnKeyInputTest()
+        {
+            Assert.AreEqual(10, player.X);
+
+            controller.OnKeyInput();
+
+            Assert.AreEqual(15, player.X);
+        }
+    }
+}
